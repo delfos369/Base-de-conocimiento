470cf76f888dbd1889dc998c3f48ccbc430fe6c7
oscar <oscar.delfos369.com>
2022-12-21 04:04:58 -0600

    blog rutas privadas

--- Tutoriales/Rutas-privadas/src/Paginas/MenuPrivado1.js
+++ Tutoriales/Rutas-privadas/src/Paginas/MenuPrivado1.js
@@ -1,9 +1,8 @@
-import React from 'react'
-
-function MenuPrivado1() {
-  return (
-    <div>Menú Privado 1</div>
-  )
-}
-
-export default MenuPrivado1
+import React from "react";
+function MenuPrivado1(){
+return (
+<div>
+<h1> Soy el Menú Privado 1 </h1>
+</div>
+)
+} export default MenuPrivado1
--- Tutoriales/Rutas-privadas/src/Validacion/ValidaRol.js
+++ Tutoriales/Rutas-privadas/src/Validacion/ValidaRol.js
@@ -1,71 +1,23 @@
+/* ValidaRol.jsx*/
+
 import React from "react";
-export let i = "";
-
-
-/* Variable que indica el rol entrará, en dado caso de no coincidir entrará al menú publico */
-export let validator = 'rol1';  
-
-export function verificaRol() {
-
-    if (validator === "rol1") {
-        i = "rol1";
-    } else if (validator === "rol2") {
-        i = "rol2";
-    }  else {
-        i = "public";
-    }
+export let i = "" ; /* Variable para comparar tipo de rol*/
+export let validator = "rol2" ; /* Variable que identifica el rol logueado*/
+export function verificaRol(){
+if ( validator === "rol" ) {
+i = "rol1" ;
+} else if ( validator === "rol2" ) {
+i = "rol2" ;
+} else {
+i = "public" ;
+}
 }
 verificaRol();
 
-
-
 export default class ValidaUsuario extends React.Component {
-    render() {
-
-
-        if (this.state.rol[0] === "admin") {
-            //   console.log("soy admin");
-            return (
-                <div>
-                    <h1>Hola Admin</h1>
-                </div>
-            )
-        } else if (this.state.rol[0] === "preventa") {
-            //    console.log("Soy Preventa");
-
-            return (
-                <div>
-                    <h1>Hola Preventa</h1>
-                </div>
-            )
-        } else if (this.state.rol[0] === "venta") {
-            //   console.log("Soy de Ventas");
-
-            return (
-                <div>
-                    <h1>Hola Ventas</h1>
-                </div>
-            )
-        } else if (this.state.rol[0] === "direccion") {
-            console.log("Soy de Dirección");
-
-            return (
-                <div>
-                    <h1>Hola Direccion</h1>
-                </div>
-            )
-        }
-
-        else {
-            console.log("ERROR");
-            return (
-                <div>
-                    <h1>Existe un Error;</h1>
-                </div>
-            )
-
-        }
-
-    }
-
+render() {
+return (
+< > </>
+)
+}
 }
